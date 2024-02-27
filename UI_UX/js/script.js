bd7534117a100c19525d39f8a316ba3bb984e2bd
MinhPhu <minhphuzcod@gmail.com>
2024-02-27 14:11:19 +0700

    Home Page Part2

---
+++ UI_UX/js/script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('#menu-bars');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+}
+
+
+var swiper = new swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
